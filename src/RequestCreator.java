a7a8a8b64c771018215c12cb790aa72970d32406
Tranthuy03 <thuy.tranthi.cit21@eiu.edu.vn>
2025-02-25 17:17:03 +0700

    Initial commit

---
+++ src/RequestCreator.java
@@ -0,0 +1,19 @@
+public abstract class RequestCreator {
+ 
+    public abstract MaintenanceRequest createRequest();
+
+    public void process() {
+        MaintenanceRequest request = createRequest();
+        request.setPriority();
+        request.setStatus();
+        request.setExpireDate();
+    
+        request.processRequest();
+
+        System.out.println("=== Request Summary ===");
+        System.out.println("Priority: " + request.getPriority());
+        System.out.println("Status: " + request.getStatus());
+        System.out.println("Expire Day: " + request.getExpireDateString());
+        System.out.println("=======================");
+    }
+}
